2df18195e22c8b4fb03c158bcc93852794be92a2
Gian Coloni <gcoloni@brasilseg.com.br>
2024-05-06 23:22:32 -0300

    feat: criando projeto

---
+++ src/main/java/com/umt/umtApplication/UmtApplication.java
@@ -0,0 +1,13 @@
+package com.umt.umtApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmtApplication.class, args);
+	}
+
+}
